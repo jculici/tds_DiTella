1733b3106ad11d33eb40d51408e7c83692c56d9e
jculici <jmculici@gmail.com>
2024-03-23 19:52:56 -0300

    Agrego que el amount provenga del valor que se le pasa al constructor

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -10,9 +10,16 @@ def test_multiplication():
     assert 10 == five.amount
 
 # If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
+def test_multiplication():
+    # test that you can multiply a Dollar by a number and get the right amount.
+    five = Dollar(5)
+    five.times(2)
+    assert 10 == five.amount
+
+# If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
 class Dollar:
     def __init__(self, amount):
-        pass
+        self.amount = amount
     
     def times(self, multiplier):
-        self.amount = 5 * 2
+        self.amount = self.amount * 2
