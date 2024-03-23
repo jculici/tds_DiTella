5ac45164c6c0384a6a0f003bf395061c2709c2c0
jculici <jmculici@gmail.com>
2024-03-23 20:18:00 -0300

    Muevo metodo eq a Money desde Dollar

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -22,13 +22,13 @@ def test_franc_multiplication():
 class Money:
     def __init__(self, amount):
         self._amount = amount
+    
+    def __eq__(self, other):
+        return self._amount == other._amount
 
 class Dollar(Money):
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
-    
-    def __eq__(self, other):
-        return self._amount == other._amount
 
 class Franc:
     def __init__(self, amount):
