aa4d67dda004d3e91f894765cfe8433b060f6eda
jculici <jmculici@gmail.com>
2024-03-23 20:14:24 -0300

    Genero clase de Franco Suizo

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -28,3 +28,13 @@ class Dollar:
     
     def __eq__(self, other):
         return self._amount == other._amount
+
+class Franc:
+    def __init__(self, amount):
+        self._amount = amount
+    
+    def times(self, multiplier):
+        return Franc(self._amount * multiplier)
+    
+    def __eq__(self, other):
+        return self._amount == other._amount
