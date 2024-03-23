080bf63e2388144176f70276809f7c816fac39c5
jculici <jmculici@gmail.com>
2024-03-23 20:14:00 -0300

    Agrego test para Francos Suizos

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -13,6 +13,11 @@ def test_equality():
     assert Dollar(5) == Dollar(5)
     assert Dollar(5) != Dollar(6)
 
+def test_franc_multiplication():
+    # test that you can multiply a Dollar by a number and get the right amount.
+    five = Franc(5)
+    assert Franc(10) == five.times(2)
+    assert Franc(15) == five.times(3)
 # If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
 class Dollar:
     def __init__(self, amount):
