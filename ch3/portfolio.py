3bdf4392e9f289433d39679be8cb47a9aeaf57b7
jculici <jmculici@gmail.com>
2024-03-23 20:12:35 -0300

    Creo un nuevo test eliminando las variables intermedias

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -6,10 +6,8 @@
 def test_multiplication():
     # test that you can multiply a Dollar by a number and get the right amount.
     five = Dollar(5)
-    ten = five.times(2)
-    assert Dollar(10) == ten 
-    fifteen = five.times(3)
-    assert Dollar(15) == fifteen
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3) 
 
 def test_equality():
     assert Dollar(5) == Dollar(5)
