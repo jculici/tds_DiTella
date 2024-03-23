2b87cc7b2d5180f09d85fc021a6f9d0569c8fe50
jculici <jmculici@gmail.com>
2024-03-23 20:19:26 -0300

    Remuevo el constructor de la clase Franc

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -33,11 +33,5 @@ class Dollar(Money):
         return Dollar(self._amount * multiplier)
     
 class Franc(Money):
-    def __init__(self, amount):
-        self._amount = amount
-    
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
-    
-    def __eq__(self, other):
-        return self._amount == other._amount
