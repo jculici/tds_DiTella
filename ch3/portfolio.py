5bd86cdc2da5f13a371eed1c8c8cbf5977d96df4
jculici <jmculici@gmail.com>
2024-03-23 20:22:56 -0300

    Agrego un metodo Dollar en Money para eliminar las clases Dollar y Francos

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -25,9 +25,12 @@ def test_franc_multiplication():
 class Money:
     def __init__(self, amount):
         self._amount = amount
-    
+
     def __eq__(self, other):
         return self._amount == other._amount and type(self) == type(other)
+
+    def dollar(amount):
+        return Dollar(amount)
 
 class Dollar(Money):
     def times(self, multiplier):
