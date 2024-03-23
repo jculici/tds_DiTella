779068282dccb1fc25af18ee61d6ee3aafe74f66
jculici <jmculici@gmail.com>
2024-03-23 19:58:40 -0300

    Creo un test para ver el efecto colateral de la multiplicacion

--- ch3/portfolio.py
+++ ch3/portfolio.py
@@ -1,34 +1,15 @@
-def test_multiplication():
+#def test_multiplication():
     # test that you can multiply a Dollar by a number and get the right amount.
-    five = Dollar(amount=5)
-    five.times(multiplier=2)
-    assert 10 == five.amount
-def test_multiplication():
-    # test that you can multiply a Dollar by a number and get the right amount.
-    five = Dollar(5)
-    five.times(2)
-    assert 10 == five.amount
-
-# If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
+#    five = Dollar(amount=5)
+#    five.times(multiplier=2)
+#    assert 10 == five.amount
 def test_multiplication():
     # test that you can multiply a Dollar by a number and get the right amount.
     five = Dollar(5)
     five.times(2)
-    assert 10 == five.amount
-
-# If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
-def test_multiplication():
-    # test that you can multiply a Dollar by a number and get the right amount.
-    five = Dollar(5)
-    five.times(2)
-    assert 10 == five.amount
-
-# If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
-def test_multiplication():
-    # test that you can multiply a Dollar by a number and get the right amount.
-    five = Dollar(5)
-    five.times(2)
-    assert 10 == five.amount
+    assert 10 == five.amount 
+    five.times(3)
+    assert 15 == five.amount
 
 # If we rewrite the 10 as 5 * 2, the duplication becomes more evident.
 class Dollar:
